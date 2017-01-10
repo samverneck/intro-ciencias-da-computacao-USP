03cccade7f526dff0c450f21a3de4c50c327f3c5
SAMUEL VERNECK <osamucaverneck@gmail.com>
2017-01-10 14:27:03 -0200

    feito

---
+++ semana-02/exercicios-de-programacao/exercicio1.py
@@ -0,0 +1,7 @@
+nomeclient = input ("Digite o nome do cliente: ") 
+contavenc = input ("Digite o dia de vencimento: ")
+mesvenc = input ("Digite o mês de vencimento: ")
+valorfat = input ("Digite o valor da fatura: ")
+
+print("Olá", nomeclient)
+print("A sua fatura com vencimento em", contavenc, mesvenc, "no valor de R$"+valorfat, "está fechada." )
